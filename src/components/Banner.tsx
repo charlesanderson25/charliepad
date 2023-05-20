5b25fe557909d7ced7e2fcf2313ba3d72506e0e4
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-20 12:41:23 -0300

    Changes Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNotebook } from "@fortawesome/free-solid-svg-icons";
+
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faNotebook} />
+    </div>
+  );
 };
 
 export default Banner;
