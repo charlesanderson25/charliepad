8094a96141fa88b25a5335ba98701263b28df0b3
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-20 15:26:57 -0300

    Changes Font Awesome

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faNotebook } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCheckSquare, faCoffee);
 
 const Banner = () => {
   return (
     <div>
-      <FontAwesomeIcon icon={faNotebook} />
+      <p>Ohhhh No!</p>
+      <FontAwesomeIcon icon="check-square" />
+      Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
     </div>
   );
 };
