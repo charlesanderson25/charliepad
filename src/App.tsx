031e23fbb53c8caca39bd8c930620b0da781dad2
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-20 11:30:13 -0300

    Clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Banner />
