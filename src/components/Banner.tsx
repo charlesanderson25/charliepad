1e7de6809a35114ef8d57e65b9d6be07af763c3f
charles <charles_spfc@hotmail.com>
2023-05-21 00:10:34 -0300

    Tailwind Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,17 @@
-import { SiMicrosoftonenote } from 'react-icons/si';
+import { SiMicrosoftonenote as IconLogoNotepad } from 'react-icons/si';
 import CreateNotepad from './CreateNotepad';
 
 const Banner = () => {
   return (
     <div style={ { fontFamily: 'Josefin Sans, sans-serif' } } className="flex-row">
       <div className="flex-row inline-block">
-        <SiMicrosoftonenote className="bg-defaultRed text-6xl	" />
-        <a href="/">Home</a>
+        
+        <a href="/" className="flex">
+          <IconLogoNotepad className="bg-defaultRed text-6xl" />
+          <h1 className="font-bold text-lg">CharliePad</h1>
+        </a>
+
+          <a href="/">Home</a>
         <CreateNotepad />
       </div>
     </div>
