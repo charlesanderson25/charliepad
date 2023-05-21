51deb2c5abf637657b2d23d4edcbb498bed5ab8c
charles <charles_spfc@hotmail.com>
2023-05-20 21:37:44 -0300

    Install React Icons

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faCheckSquare, faCoffee} from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 library.add(faCheckSquare, faCoffee);
 
@@ -10,7 +11,7 @@ const Banner = () => {
       <p>Ohhhh No!</p>
       <FontAwesomeIcon icon="check-square" />
       Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
-      <FontAwesomeIcon icon={faCoffee} />
+      <FontAwesomeIcon icon={faFacebook} />
     </div>
   );
 };
