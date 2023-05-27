dbfbdfb8d8d5464e08df9abdbe7d876e4b4a206d
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-27 16:20:43 -0300

    Add useState on input fields

--- src/routes/CreateNotepadRoute.tsx
+++ src/routes/CreateNotepadRoute.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import ButtonSubmitForm from "../components/ButtonSubmitForm";
+import { useState } from "react";
 
 interface StyledLabelProps {
   titulo: string;
@@ -16,16 +17,31 @@ const StyledLabel = styled.label<StyledLabelProps>`
 `;
 
 const CreateNotepadRoute = () => {
+  const [title, setTittle] = useState("");
+  const [subtittle, setSubtittle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <section className="my-20 relative min-h-screen">
       <div className="form-container absolute inset-x-1/4">
-        <form className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans">
+        <form
+          className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans"
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
           <h1 className="font-bold text-2xl text-white ml-3">Criar Notepad</h1>
           <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
             <input
               className="bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md "
               type="text"
               placeholder="Digite seu Título"
+              value={title}
+              onChange={(event) => {
+                const newTittle = event.target.value;
+                setTittle(newTittle);
+                console.log(newTittle);
+              }}
             />
           </StyledLabel>
           <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
@@ -33,6 +49,12 @@ const CreateNotepadRoute = () => {
               className="bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md"
               type="text"
               placeholder="Digite o Subtítulo"
+              value={subtittle}
+              onChange={(event) => {
+                const newSubtittle = event.target.value;
+                setSubtittle(newSubtittle);
+                console.log(newSubtittle);
+              }}
             />
           </StyledLabel>
           <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
@@ -41,6 +63,12 @@ const CreateNotepadRoute = () => {
               name=""
               id=""
               placeholder="Digite seu Texto"
+              value={content}
+              onChange={(event) => {
+                const newContent = event.target.value;
+                setContent(newContent);
+                console.log(newContent);
+              }}
             ></textarea>
           </StyledLabel>
           <button type="submit">{<ButtonSubmitForm />}</button>
