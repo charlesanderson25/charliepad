8905b775a9a405537f66061d78140541991124b0
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-27 10:35:11 -0300

    Add StyledLabel

--- src/routes/CreateNotepadRoute.tsx
+++ src/routes/CreateNotepadRoute.tsx
@@ -1,35 +1,47 @@
+import styled from "styled-components";
+
+interface StyledLabelProps {
+  titulo: string;
+  subtitulo: string;
+  texto: string;
+}
+
+const StyledLabel = styled.label<StyledLabelProps>`
+  display: block;
+  font-size: 2rem;
+  color: white;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
 const CreateNotepadRoute = () => {
   return (
     <section className="my-20 relative min-h-screen">
       <div className="form-container absolute inset-x-1/4">
         <form className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans">
           <h1 className="font-bold text-2xl text-white">Criar Notepad</h1>
-          <label className="block my-4 text-2xl text-white">
-            Título
+          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
             <input
               className="bg-darkTheme shadow-md w-full py-3 px-1 m-2 border rounded-md"
               type="text"
               placeholder="Digite seu Título"
             />
-          </label>
-          <label className="block my-4 text-2xl text-white">
-            {" "}
-            Subtítulo
+          </StyledLabel>
+          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
             <input
               className="bg-darkTheme shadow-md w-full py-3 px-1 m-2 border rounded-md	"
               type="text"
               placeholder="Digite o Subtítulo"
             />
-          </label>
-          <label className="block my-4 text-2xl text-white">
-            Texto
+          </StyledLabel>
+          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
             <textarea
               className="bg-darkTheme shadow-md w-full py-3 px-1 m-2 border rounded-md	"
               name=""
               id=""
               placeholder="Digite seu Texto"
             ></textarea>
-          </label>
+          </StyledLabel>
           <button type="submit">Enviar</button>
         </form>
       </div>
