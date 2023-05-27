af01a58335e25f428c608770feee821c9ae9622c
charles <charles_spfc@hotmail.com>
2023-05-26 23:50:26 -0300

    Tailwind CreateNotepadRoute

--- src/components/routes/CreateNotepadRoute.tsx
+++ src/components/routes/CreateNotepadRoute.tsx
@@ -1,14 +1,22 @@
 const CreateNotepadRoute = () => {
     return (
-        <div>
-            <form>
-                <h1>Criar Notepad</h1>
-                <input type="text" placeholder="Digite seu Título" />
-                <input type="text" placeholder="Digite o Subtítulo" />
-                <textarea name="" id="" placeholder="Digite seu Texto"></textarea>
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
+        <section className="my-20 relative min-h-screen">
+            <div className="form-container absolute inset-x-1/4">
+                <form className="form bg-gray-200 rounded-2xl py-9 px-16 font-sans">
+                    <h1 className="font-bold text-2xl">Criar Notepad</h1>
+                    <label className="block my-4 text-2xl ">Título
+                        <input className="bg-white shadow-md w-full py-3 px-1 m-2" type="text" placeholder="Digite seu Título" />
+                    </label>
+                    <label className="block my-4 text-2xl "> Subtítulo
+                        <input className="bg-white shadow-md w-full py-3 px-1 m-2" type="text" placeholder="Digite o Subtítulo" />
+                    </label>
+                    <label className="block my-4 text-2xl ">Texto
+                        <textarea className="bg-white shadow-md w-full py-3 px-1 m-2" name="" id="" placeholder="Digite seu Texto"></textarea>
+                    </label>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
