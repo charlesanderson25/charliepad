7749fa3d7b98e819a05f0ac1cae8cf7292213c83
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-28 15:49:30 -0300

    Add getNotepads function

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,50 +1,77 @@
-// import NotepadBody from "../routes/HomeRoute";
-
-// const HomeRoute = () => {
-//     return (
-//         <div>
-//             <NotepadBody/>
-//         </div>
-//     )
-// }
-
-// export default HomeRoute;
-
 import uuid from "react-uuid";
+import axios from "axios";
+import { useState } from "react";
+import { api } from "../api";
 
-const notepads = [
-  {
-    id: uuid(),
-    tittle: "Primeiro Notepad",
-    subtitle: "Primeiro Notepad cadastrado",
-    created_at: new Date(),
-  },
+// const notepads = [
+//   {
+//     id: uuid(),
+//     tittle: "Primeiro Notepad",
+//     subtitle: "Primeiro Notepad cadastrado",
+//     created_at: new Date(),
+//   },
 
-  {
-    id: uuid(),
-    tittle: "Segundo Notepad",
-    subtitle: "Segundo Notepad cadastrado",
-    created_at: new Date(),
-  },
-];
+//   {
+//     id: uuid(),
+//     tittle: "Segundo Notepad",
+//     subtitle: "Segundo Notepad cadastrado",
+//     created_at: new Date(),
+//   },
+// ];
+
+interface Notepad {
+  id: string;
+  title: string;
+  subtitle: string;
+  content: string;
+  created_at: Date;
+}
+
+const initialNotepads: Notepad[] = [];
 
 // Componente Filho
 const NotepadList = () => {
+  const [notepads, setNotepads] = useState<Notepad[]>(initialNotepads);
+
+  async function getNotepads() {
+    const response = await api.get("/notepads");
+    const showNotepads: Notepad[] = response.data.notepads;
+    setNotepads(showNotepads);
+  }
+
+  getNotepads();
+
   return (
     <div className="p-5 m-5 text-white bg-darkTheme">
       <h2>NotepadList</h2>
       {notepads.map((notepad) => {
         return (
           <div key={notepad.id} className="border-b p-2">
-            <h1 className="text-lg font-bold">{notepad.tittle}</h1>
+            <h1 className="text-lg font-bold">{notepad.title}</h1>
             <h4>{notepad.subtitle}</h4>
-            <p>{notepad.created_at.toLocaleDateString()}</p>
+            <p>{new Date(notepad.created_at).toLocaleDateString()}</p>
           </div>
         );
       })}
     </div>
   );
 };
+
+// return (
+//   <div className="p-5 m-5 text-white bg-darkTheme">
+//     <h2>NotepadList</h2>
+//     {notepads.map((notepad) => {
+//       return (
+//         <div key={notepad.id} className="border-b p-2">
+//           <h1 className="text-lg font-bold">{notepad.tittle}</h1>
+//           <h4>{notepad.subtitle}</h4>
+//           <p>{notepad.created_at.toLocaleDateString()}</p>
+//         </div>
+//       );
+//     })}
+//   </div>
+// );
+// };
 
 // Componente Pai
 const HomeRoute = () => {
