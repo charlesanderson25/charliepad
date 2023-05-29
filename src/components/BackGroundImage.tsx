5bf13a00bfe9ebf7c970ddacc91eaa629c3fcc3e
charles <charles_spfc@hotmail.com>
2023-05-28 22:12:48 -0300

    Changes Welcome

--- src/components/BackGroundImage.tsx
+++ src/components/BackGroundImage.tsx
@@ -1,7 +1,18 @@
+import fundoPretoTextura from "../assets/fundoPretoTextura.jpg";
+
+const ImageBackground = () =>{
+    return (
+        <div style={ { backgroundImage: `url(${fundoPretoTextura})` } }>
+
+        </div>
+    );
+}
+
+
 const BackGroundImage = () =>{
     return (
         <div>
-
+            <ImageBackground/>
         </div>
     )
 }
--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -1,16 +1,21 @@
+import BackGroundImage from "../components/BackGroundImage";
+
 const Welcome = () => {
     return (
-        <div className="p-20">
-            <h2 
-                style={{ fontFamily: "Josefin Sans, sans-serif" }} 
-                className="text-white text-5xl font-extrabold tracking-wide">
-                Bem vindo ao <span className="text-defaultRed">CharliePad</span>
-            </h2>
-            <p className="text-white font-bold tracking-wide pl-1 pt-2">
-               Faça suas anotações, relate seus pensamentos, tome notas. . .  
-            </p>
-        </div>
+
+            <div className="p-20">
+                <h2 
+                    style={{ fontFamily: "Josefin Sans, sans-serif" }} 
+                    className="text-white text-5xl font-extrabold tracking-wide">
+                    Bem vindo ao <span className="text-defaultRed">CharliePad</span>
+                </h2>
+                <p className="text-white font-bold tracking-wide pl-1 pt-2">
+                Faça suas anotações, relate seus pensamentos, tome notas. . .  
+                </p>
+            </div>
+        
     )
 }
 
-export default Welcome
+export default Welcome;
+
