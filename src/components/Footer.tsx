d528620fd3a55abe98373224e05f1ec2eb5758d9
charles <charles_spfc@hotmail.com>
2023-05-29 20:47:32 -0300

    Changes componente style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,8 @@ import { FaRegCopyright } from 'react-icons/fa';
 
 const Footer = () =>{
     return (
-        <div>
-            <footer> 
-                <h2 className='flex justify-center'><FaRegCopyright />Charles Anderson</h2>
-            </footer>
+        <div className="flex justify-center">
+            <h2 ><FaRegCopyright />Charles Anderson</h2>
         </div>
     )
 }
