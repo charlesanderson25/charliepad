b6c6de1b2a2d5b7a098f93646949fbf0ff52193a
charles <charles_spfc@hotmail.com>
2023-05-29 20:46:51 -0300

    Changes Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Banner from "./components/Banner";
-import HomeRoute from "../src/routes/HomeRoute";
-import CreateNotepadRoute from "../src/routes/CreateNotepadRoute";
 import Footer from "./components/Footer";
-import Welcome from "./routes/Welcome";
-import BackGroundImage from "./components/BackGroundImage";
+import RoutesApp from "./routes/routes";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <div>
-          <Banner />
-
-          <Routes>
-            <Route path="/Welcome" element={<Welcome />} />
-            <Route path="/" element={<HomeRoute />} />
-            <Route path="/Criar-Notepad" element={<CreateNotepadRoute />} />
-            <Route path="Image" element={<BackGroundImage/>}/>
-          </Routes>
-        </div>
+      <Banner />
+      <RoutesApp/>
+      <div className="block bg-green-500">
         <Footer />
-
-      </BrowserRouter>
+      </div>
     </>
   );
 }
