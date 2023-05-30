b5cf97fe1d3d998d1e92bd67871b64bcb6966570
charles <charles_spfc@hotmail.com>
2023-05-30 18:53:27 -0300

    Add ListNotepads component

---
+++ src/components/ListNotepads.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const ListNotepads = () =>{
+    return (
+        <div>
+            <Link to="/listar-notepads" className="pl-12">
+            <button className="text-white text-l font-bold hover:text-defaultRed">
+                Listar Notepads
+            </button>
+            </Link>
+        </div>
+    )
+}
+
+export default ListNotepads;
