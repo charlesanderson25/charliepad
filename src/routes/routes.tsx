da7f963305134bed27f4316b338ebc2de8557d02
charles <charles_spfc@hotmail.com>
2023-05-30 18:31:28 -0300

    Update routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import BackGroundImage from "../components/BackGroundImage";
 import { NotFoundTwo } from "../pages/notFound/NotFoundTwo";
-import CreateNotepadRoute from "../pages/notFound/CreateNotepadRoute/CreateNotepadRoute";
+
 import HomeRoute from "../pages/homeRoute/HomeRoute";
 import Welcome from "../pages/welcome/Welcome";
+import CreateNotepadRoute from "../pages/createNotepadRoute/CreateNotepadRoute";
 
 const RoutesApp = () => {
     return (
