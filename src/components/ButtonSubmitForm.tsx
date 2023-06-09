081c865d56c8ef48ad93113404535db289703f1d
charles <charles_spfc@hotmail.com>
2023-06-09 20:07:40 -0300

    Add type="submit"

--- src/components/ButtonSubmitForm.tsx
+++ src/components/ButtonSubmitForm.tsx
@@ -4,7 +4,7 @@ const ButtonSubmitForm = () => {
       style={{ fontFamily: "Josefin Sans, sans-serif" }}
       className="flex ml-4 mt-4 md:mt-0"
     >
-      <button className="rounded-lg bg-defaultRed font-bold text-lg p-3 border-none cursor-pointer text-white hover:text-hoverdefaultRed">
+      <button type="submit" className="rounded-lg bg-defaultRed font-bold text-lg p-3 border-none cursor-pointer text-white hover:text-hoverdefaultRed">
         Enviar
       </button>
     </div>
