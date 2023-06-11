d4ca7ba9d9af8e1c8fdc190ec2d224c3ce712e3a
charles <charles_spfc@hotmail.com>
2023-06-10 21:38:31 -0300

    Change nome component HomeRoute > NotepadListMain

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import BackGroundImage from "../components/BackGroundImage";
 import { NotFoundTwo } from "../pages/notFound/NotFoundTwo";
-import HomeRoute from "../pages/homeRoute/HomeRoute";
+import HomeRoute from "../pages/NotepadListMain/NotepadListMain";
 import Welcome from "../pages/welcome/Welcome";
 import CreateNotepadRoute from "../pages/createNotepadRoute/CreateNotepadRoute";
 import EditNotepadRoute from "../pages/editNotepad/EditNotepadRoute";
+import NotepadListMain from "../pages/NotepadListMain/NotepadListMain";
 
 const RoutesApp = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="/listar-notepads" element={<HomeRoute />} />
+      <Route path="/listar-notepads" element={<NotepadListMain />} />
       <Route path="/criar-Notepad" element={<CreateNotepadRoute />} />
       <Route path="image" element={<BackGroundImage />} />
       <Route path="/editar-notepad/:id" element={<EditNotepadRoute />} />
