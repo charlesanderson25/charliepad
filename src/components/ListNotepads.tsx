2a97e8a141c4ee9d7a1520cbef6306e7b5cfba22
charles <charles_spfc@hotmail.com>
2023-06-10 21:29:41 -0300

    Add textListNotepads

--- src/components/ListNotepads.tsx
+++ src/components/ListNotepads.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
+const textListNotepads = {
+    button: "Listar Notepads",
+}
+
 const ListNotepads = () =>{
     return (
         <div>
             <Link to="/listar-notepads" className="pl-12">
             <button className="text-white text-l font-bold hover:text-defaultRed tracking-wide">
-                Listar Notepads
+                {textListNotepads.button}
             </button>
             </Link>
         </div>
