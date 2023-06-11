1b7709b8d12daf41a1b7c877e51c9dfb9d4c628c
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-11 09:49:51 -0300

    Add useEffect

--- src/pages/createNotepadRoute/CreateNotepadRoute.tsx
+++ src/pages/createNotepadRoute/CreateNotepadRoute.tsx
@@ -8,11 +8,12 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-simple-toasts";
 import BreadCrumbs from "../../components/BreadCrumbs";
 import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 
 const textCreateNotepadRoute = {
   h1: "Criar Notepad",
   title: "Criar Notepad",
-}
+};
 
 interface StyledLabelProps {
   titulo: string;
@@ -46,17 +47,18 @@ const CreateNotepadRoute = () => {
     },
   });
 
+  useEffect(() => {
+    console.log("CreateNotepadRoute renderizado com sucesso!");
+  }, []);
+
   return (
     <section className="my-20 relative min-h-screen">
       <Helmet>
-        <title>
-          {textCreateNotepadRoute.title}
-        </title>
+        <title>{textCreateNotepadRoute.title}</title>
       </Helmet>
       <div className="form-container absolute inset-x-1/4">
         <form
           className="form bg-darkTheme rounded-2xl py-9 px-16 font-sans"
-        
           ref={zo.ref}
         >
           <div className="text-white pl-3 pb-1">
@@ -70,54 +72,69 @@ const CreateNotepadRoute = () => {
               ]}
             />
           </div>
-          <h1 className="font-bold text-2xl text-white ml-3"> {textCreateNotepadRoute.h1} </h1>
-          
+          <h1 className="font-bold text-2xl text-white ml-3">
+            {" "}
+            {textCreateNotepadRoute.h1}{" "}
+          </h1>
+
           <div className="flex-col">
-          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
-            </StyledLabel>
+            <StyledLabel
+              titulo="Título"
+              subtitulo="Subtítulo"
+              texto="Texto"
+            ></StyledLabel>
             <input
-              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.title("border-2 border-red-500 focus:border-red-600")} `}
+              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.title(
+                "border-2 border-red-500 focus:border-red-600"
+              )} `}
               type="text"
               placeholder="Digite seu Título"
-              
               name={zo.fields.title()}
-              />
-          {zo.errors.title((erro) => (
-            <ErrorMessage>{erro.message}</ErrorMessage>
-          ))}
+            />
+            {zo.errors.title((erro) => (
+              <ErrorMessage>{erro.message}</ErrorMessage>
+            ))}
           </div>
-          
+
           <div className="flex-col">
-          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
-          </StyledLabel>
-          
-            <input className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.subtitle("border-2 border-red-500 focus:border-red-600")} `}
+            <StyledLabel
+              titulo="Título"
+              subtitulo="Subtítulo"
+              texto="Texto"
+            ></StyledLabel>
+
+            <input
+              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.subtitle(
+                "border-2 border-red-500 focus:border-red-600"
+              )} `}
               type="text"
               placeholder="Digite o Subtítulo"
               name={zo.fields.subtitle()}
-           />
+            />
 
-          {zo.errors.subtitle((erro) => (
-            <ErrorMessage>{erro.message}</ErrorMessage>
-          ))}
+            {zo.errors.subtitle((erro) => (
+              <ErrorMessage>{erro.message}</ErrorMessage>
+            ))}
           </div>
-          
 
           <div className="flex-col">
-          <StyledLabel titulo="Título" subtitulo="Subtítulo" texto="Texto">
-            </StyledLabel>
+            <StyledLabel
+              titulo="Título"
+              subtitulo="Subtítulo"
+              texto="Texto"
+            ></StyledLabel>
             <textarea
-              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.content("border-2 border-red-500 focus:border-red-600")} `}
-              
+              className={`bg-backGroundColorDarkTheme shadow-md w-full py-3 px-1 m-2 rounded-md text-white ${zo.errors.content(
+                "border-2 border-red-500 focus:border-red-600"
+              )} `}
               id=""
               placeholder="Digite seu Texto"
-              
               name={zo.fields.content()}
             ></textarea>
 
-          {zo.errors.content((erro) => (
-            <ErrorMessage>{erro.message}</ErrorMessage>
-          ))}
+            {zo.errors.content((erro) => (
+              <ErrorMessage>{erro.message}</ErrorMessage>
+            ))}
           </div>
           <ButtonSubmitForm />
         </form>
@@ -127,4 +144,3 @@ const CreateNotepadRoute = () => {
 };
 
 export default CreateNotepadRoute;
-
